69620fb9012e2cf4140d1979ae1f2bd3902d393c
beiluonever <beiluo1994@gmail.cm>
2019-12-16 10:27:35 +0800

    fix application test is not public.

--- flowable/src/test/java/net/pyma/demo/flowable/FlowableApplicationTests.java
+++ flowable/src/test/java/net/pyma/demo/flowable/FlowableApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlowableApplicationTests {
+public class FlowableApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
